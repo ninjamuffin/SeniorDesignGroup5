90df0e56e9dca7d48daa05bd47aa4ab4542f79e9
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2015-12-06 20:38:59 -0800

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+ 
+
+<?php
+$con = mysqli_connect('us-cdbr-azure-west-c.cloudapp.net','b2a3214e88e413','325ebc40','mysqldbproject');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+$sql="INSERT INTO nametable (FirstName, LastName, Age, Hometown, Job)
+VALUES
+('$_POST[FirstName]','$_POST[LastName]','$_POST[Age]','$_POST[Hometown]','$_POST[Job]')";
+ 
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+ 
+mysql_close($con)
+?>
+</body>
+</html>
