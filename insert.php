24319c13e0bec91f1708daf6b9f257080325d066
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2015-12-06 22:44:17 -0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,12 +7,11 @@ if (!$con) {
 // Escape user inputs for security
 $first_name = mysqli_real_escape_string($link, $_POST['firstname']);
 $last_name = mysqli_real_escape_string($link, $_POST['lastname']);
-$age = mysqli_real_escape_string($link, $_POST['age']);
 $hometown = mysqli_real_escape_string($link, $_POST['hometown']);
 $job = mysqli_real_escape_string($link, $_POST['job']);
  
 // attempt insert query execution
-$sql = "INSERT INTO persons (FirstName, LastName, Age, Hometown, Job) VALUES ('$first_name', '$last_name', '$age', '$hometown', '$job')";
+$sql = "INSERT INTO persons (FirstName, LastName, Hometown, Job) VALUES ('$first_name', '$last_name', '$hometown', '$job')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
 } else{
