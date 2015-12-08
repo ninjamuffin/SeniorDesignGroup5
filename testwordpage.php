2fc2845f9472dd9f4b5a65cfcce8116680704b74
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2015-12-08 15:04:37 -0800

    Update testwordpage.php

--- testwordpage.php
+++ testwordpage.php
@@ -23,8 +23,30 @@ $con = mysqli_connect('us-cdbr-azure-west-c.cloudapp.net','b2a3214e88e413','325e
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
+mysqli_select_db($con,"mysqldbproject");
+$sql="SELECT * FROM expressions
+      WHERE expressions LIKE '%the%'";
+echo ($sql);
+$result = mysqli_query($con,$sql);
 echo ($q);
-
+echo "<table>
+<tr>
+<th>student_id</th>
+<th>expression</th>
+<th>level_id</th>
+<th>topic_id</th>
+<th>language_id</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['student_id'] . "</td>";
+    echo "<td>" . $row['expression'] . "</td>";
+    echo "<td>" . $row['level_id'] . "</td>";
+    echo "<td>" . $row['topic_id'] . "</td>";
+    echo "<td>" . $row['language_id'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
 
 
 mysqli_close($con);
