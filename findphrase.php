368889d3fe48577fe6988732075af6a911974b0d
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2016-01-19 15:02:53 -0800

    Update findphrase.php

--- findphrase.php
+++ findphrase.php
@@ -30,7 +30,6 @@ $con = mysqli_connect('us-cdbr-azure-west-c.cloudapp.net','b2a3214e88e413','325e
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
-echo $q;
 //Search results for echo ($q);
 mysqli_select_db($con,"mysqldbproject");
 $sql= "SELECT * FROM expressions_full WHERE expression LIKE '%{$q}%'";
