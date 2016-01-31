349b358c7a016156b17ccb3d63cbc44e54378834
BrendanKennedy <bkennedy1704@gmail.com>
2016-01-30 22:09:22 -0800

    corpus home page (index.html) and customized sidebar

---
+++ corpus/index.php
@@ -0,0 +1,117 @@
+<?php include "/base.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title> Gonzaga Small Talk</title>
+
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/simple-sidebar.css" rel="stylesheet">
+
+
+    <!-- Including Header -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script>
+        $(function(){
+            $("#header").load("/header.php");
+        });
+    </script>
+
+    <!-- Background Setup -->
+    <style>
+        body{
+            background: url(/Media/gonzagasmalltalk_background.png) no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: auto;
+        }
+    </style>
+</head>
+
+<?php
+session_start();
+if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
+{
+    if($_SESSION['Role'] != 'admin')
+    {
+        ?>
+        <p>You do not have permission to view this page.  Redirecting in 5 seconds</p>
+        <p>Click <a href="/index.php">here</a> if you don't want to wait</p>
+        <meta http-equiv='refresh' content='5;/index.php' />
+        <?php
+    }
+    else
+    {
+        ?>
+        <body>
+            <div id="header"></div>
+
+            <div id="wrapper">
+                <div id="sidebar-wrapper">
+                    <u1 class="sidebar-nav">
+                        <li class="sidebar-brand">
+                            <a href="/<?=$_SESSION['Role']?>/Home/index.php">Home</a>
+                        </li>
+                        <li class="sidebar-brand">
+                            <a href="#">Tag Search</a>
+                        </li>
+                        <li class="sidebar-brand">
+                            <a href="#">Annotation Editor</a>
+                        </li>
+                        <li class="sidebar-brand">
+                            <a href="#">Graphical Analysis</a>
+                        </li>
+
+                        <li class="sidebar-brand">
+                            <a href="#">Help</a>
+                        </li>
+
+                    </u1>    
+
+
+                </div>
+                <div id="sidebar-content-wrapper">
+                    <div class="col-sm-12">
+                        <div class="container">
+                            <h1>Corpus Search Home</h1>
+                            <p>Documentation:</p>
+                            <p>Only accessible to teacher and user accounts.  Home search page for corpus, will provide links to other types of corpus access/search</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="/js/bootstrap.min.js"></script>
+            <script src="/js/bootstrap-datepicker.js"></script>
+            <script>
+            $("#menu-toggle").click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+            </script>
+        </body> 
+        <?php        
+    }
+}
+
+else
+{
+    ?>
+    <p>Oops! You are not logged in.  Redirecting to log-in in 5 seconds</p>
+    <p>Click <a href="/index.php">here</a> if you don't want to wait</p>
+    <meta http-equiv='refresh' content='5;/index.php' />
+    <?php
+}
+?>
+
+</html>
