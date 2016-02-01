e6c078da39ff5d88c47063cb23a45b7e13e12679
BrendanKennedy <bkennedy1704@gmail.com>
2016-01-31 17:22:43 -0800

    Debugging student/home p2

--- student/home/index.php
+++ student/home/index.php
@@ -89,5 +89,15 @@ if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
     </body>
     <?php
     }
+}
+    
+else
+{
     ?>
+    <p>Oops! You are not logged in.  Redirecting to log-in in 5 seconds</p>
+    <p>Click <a href="/index.php">here</a> if you don't want to wait</p>
+    <meta http-equiv='refresh' content='5;/index.php' />
+    <?php
+}
+?>
 </html>
