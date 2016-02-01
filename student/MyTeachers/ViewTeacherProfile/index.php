c484b4554bb6ddd43450736b948ffd754c6e3e46
Kevin Wisdom <kwisdom@zagmail.gonzaga.edu>
2016-01-31 16:09:47 -0800

    adding new php bases for student section

---
+++ student/MyTeachers/ViewTeacherProfile/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Gonzaga Smalltalk</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script>
+        $(function(){
+            $("#header").load("/header.html");
+        });
+    </script>
+</head>
+
+<body background="/media/gonzagasmalltalk_background.png">
+    <div id="header"></div>
+
+        <div class="jumbotron">
+            <div class="container">
+                <p>Documentation:</p>
+                <p>For the student user, this page will provide a viewable profile for a given teacher, displaying scheduling, office hours, contact information, and active courses</p>
+                <p>Standard student navbars</p>
+
+            </div>
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/bootstrap-datepicker.js"></script>
+      </body>
+</html>
---
+++ student/home/Profile/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Gonzaga Smalltalk</title>
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script>
+        $(function(){
+            $("#header").load("/header.html");
+        });
+    </script>
+</head>
+
+<body background="/media/gonzagasmalltalk_background.png">
+    <div id="header"></div>
+
+        <div class="jumbotron">
+            <div class="container">
+                <p>Documentation:</p>
+                <p>Page will provide the student user the ability to review one's own profile.  In the first form, this will include contact information and active courses.  In the advanced form, this will include progress track and graphical representation of progress.</p>
+                <p>Standard student sidebar</p>
+                <p>Standard header navigation bar</p>
+
+            </div>
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/bootstrap-datepicker.js"></script>
+      </body>
+</html>
