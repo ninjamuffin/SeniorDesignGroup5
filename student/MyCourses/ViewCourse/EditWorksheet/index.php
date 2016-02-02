50d6406fcdb7c1daed19da36db550cff69151440
BrendanKennedy <bkennedy1704@gmail.com>
2016-02-01 19:40:38 -0800

    Fixing some errors in student files

--- student/MyCourses/ViewCourse/EditWorksheet/index.php
+++ student/MyCourses/ViewCourse/EditWorksheet/index.php
@@ -82,6 +82,12 @@ if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="/js/bootstrap.min.js"></script>
+        <script>
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });
+        </script>
       </body>
     <?php
     }
