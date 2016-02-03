e9cf4a3e314e605f7fa9be8b7738f4c338d70728
BrendanKennedy <bkennedy1704@gmail.com>
2016-02-02 20:42:38 -0800

    Debugging createCourse

--- admin/ManageCourses/CreateCourse/index.php
+++ admin/ManageCourses/CreateCourse/index.php
@@ -61,7 +61,7 @@ if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
         //$Location = $_POST['location'];
         $query = "SELECT * FROM Session WHERE Session = 'Spring I'";
         $params = array();
-        $options = array( "Scrollable" => "keyset" );
+        $options = array( "Scrollable" => "buffered" );
         $stmt = sqlsrv_query($con, $query, $params, $options);
         $row_count = sqlsrv_num_rows($stmt);
         
