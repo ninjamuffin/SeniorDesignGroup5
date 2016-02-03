072e714148008527b2e4515ad8118dbf4cdbebfc
BrendanKennedy <bkennedy1704@gmail.com>
2016-02-02 20:01:37 -0800

    Debugging createCourse

--- admin/ManageCourses/CreateCourse/index.php
+++ admin/ManageCourses/CreateCourse/index.php
@@ -60,7 +60,7 @@ if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
         //$ClassName = $_POST['instructorLastName'];
         //$CRN = $_POST['CRN'];
         //$Location = $_POST['location'];
-        $getSessionsID = "SELECT * FROM Session WHERE Session = 'Spring I'";
+        $getSessionsID = "SELECT * FROM Session WHERE Session = 'Spring I';";
         ?>
         <p><?=$getSessionsID?></p>
         <?php
