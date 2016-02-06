e1a80b1dc4bc1c3cd3dc138fc48ded5197f1291f
BrendanKennedy <bkennedy1704@gmail.com>
2016-02-05 19:50:42 -0800

    New file: admin/archive/courses/viewcourse takes POST from courses and generates course page

--- admin/Archive/Courses/ViewCourse/index.php
+++ admin/Archive/Courses/ViewCourse/index.php
@@ -1,6 +1,6 @@
 <?php 
-include "../../../base.php"; 
-include "../../../pagination.php";
+include "../../../../base.php"; 
+include "../../../../pagination.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
