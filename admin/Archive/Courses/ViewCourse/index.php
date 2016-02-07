5e40de3a2be83b01c81be77dd1a30d007c24da09
BrendanKennedy <bkennedy1704@gmail.com>
2016-02-06 16:38:11 -0800

    Changing menu-toggle to glyphicon menu-burger

--- admin/Archive/Courses/ViewCourse/index.php
+++ admin/Archive/Courses/ViewCourse/index.php
@@ -63,10 +63,11 @@ if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
                 <div id="sidebar"></div>
                 <div id="page-content-wrapper">
                     <div class="container-fluid">
-                        <a href="#menu-toggle" id="menu-toggle"><span class="glyphicon glyphicon-menu-burger"></span> </a>
+                        
                         <div class="row">
                             <div class="col-lg-12">
                                 <!--<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Collapse/Expand</a>-->
+                                <a href="#menu-toggle" id="menu-toggle"><span class="glyphicon glyphicon-menu-burger"></span> </a>
                                 <div class="panel panel-primary">
                                     <div class="panel-heading">Documentation</div>
                                     <div class="panel-body">
