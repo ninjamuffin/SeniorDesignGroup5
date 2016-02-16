e09f72fc5dbb9b01d73d2b88ead3ea0588f257c0
BrendanKennedy <bkennedy1704@gmail.com>
2016-02-15 19:43:56 -0800

    Starting work on searching for a course from an admin account

--- admin/Archive/Courses/Search/index.php
+++ admin/Archive/Courses/Search/index.php
@@ -63,13 +63,7 @@ if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
                         <div class="row">
                             <div class="col-lg-12">
                                 <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Collapse/Expand</a>
-                                <div class="panel panel-primary">
-                                    <div class="panel-heading">Search for a course</div>
-                                    <div class="panel-body">
-                                        
-                                    </div>
                                 
-                                </div>
                                 <div class="panel panel-primary">
                                     <div class="panel-heading">Course Listing (sort by most recent)</div>
                                     <!-- Select Rows Per Page -->
