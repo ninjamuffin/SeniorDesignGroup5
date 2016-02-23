4c417204a487a70dca81239ae537b6e0b16d094a
Kevin Wisdom <kwisdom@zagmail.gonzaga.edu>
2016-02-23 11:59:29 -0800

    fixing jquery include calls to always be https in root directory

--- register.php
+++ register.php
@@ -12,7 +12,7 @@
         <link href="css/bootstrap.css" rel="stylesheet">
         
         <!-- Header File -->
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script>
             $(function(){
                 $("#header").load("header.html");
