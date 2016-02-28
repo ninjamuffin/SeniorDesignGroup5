d800e8771794884784ed16fc9e247cdba78694e7
Kevin Wisdom <kwisdom@zagmail.gonzaga.edu>
2016-02-28 14:53:04 -0800

    fixed https:// js calls

--- admin/Home/index.php
+++ admin/Home/index.php
@@ -15,7 +15,7 @@
 
 
     <!-- Including Header -->
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script>
         $(function(){
             $("#header").load("/header.php");
