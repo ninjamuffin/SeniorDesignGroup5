441382cc07c17f57bd92b06ff8f8b9d9f92cc9f7
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2016-03-01 19:55:28 -0800

    Update findphrase.php

--- corpus/findphrase.php
+++ corpus/findphrase.php
@@ -1,56 +1,18 @@
 <?php
-class DBController {
-	private $host = "us-cdbr-azure-west-c.cloudapp.net";
-	private $user = "b2a3214e88e413";
-	private $password = "325ebc40";
-	private $database = "mysqldbproject";
-	
-	function __construct() {
-		$conn = $this->connectDB();
-		if(!empty($conn)) {
-			$this->selectDB($conn);
-		}
-	}
-	
-	function connectDB() {
-		$conn = mysql_connect($this->host,$this->user,$this->password);
-		return $conn;
-	}
-	
-	function selectDB($conn) {
-		mysql_select_db($this->database,$conn);
-	}
-	
-	function runQuery($query) {
-		$result = mysql_query($query);
-		while($row=mysql_fetch_assoc($result)) {
-			$resultset[] = $row;
-		}		
-		if(!empty($resultset))
-			return $resultset;
-	}
-	
-	function numRows($query) {
-		$result  = mysql_query($query);
-		$rowcount = mysql_num_rows($result);
-		return $rowcount;	
-	}
+if (isset($_POST['search'])) {
+        $search = htmlentities($_POST['search']);
+ 
+$db = mysql_connect('us-cdbr-azure-west-c.cloudapp.net','b2a3214e88e413','325ebc40'); //Don't forget to change
+mysql_select_db('mysqldbproject', $db);             //theses parameters
+$sql = "SELECT expression from expressions_full WHERE expression LIKE '$search%'";
+$req = mysql_query($sql) or die();
+echo '<ul>';
+while ($data = mysql_fetch_array($req))
+{
+      echo '<li><a href="#" onclick="selected(this.innerHTML);">'.htmlentities($data['expression']).'</a></li>';
+}
+echo '</ul>';
+mysql_close();
+exit;
 }
 ?>
-<!--"SELECT * FROM expressions_full WHERE expression LIKE '%{$q}%'";-->
-<?php
-//require_once("dbcontroller.php");
-$db_handle = new DBController();
-if(!empty($_POST["keyword"])) {
-$query =""SELECT * FROM expressions_full WHERE expression LIKE '" . $_POST["keyword"] . "%'";
-$result = $db_handle->runQuery($query);
-if(!empty($result)) {
-?>
-<ul id="country-list">
-<?php
-foreach($result as $country) {
-?>
-<li onClick="selectCountry('<?php echo $country["expression"]; ?>');"><?php echo $country["expression"]; ?></li>
-<?php } ?>
-</ul>
-<?php } } ?>
