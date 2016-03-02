61d9ce4ebc74ef3a445f4a375a737c1c6ca174d6
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2016-03-01 20:11:55 -0800

    Update findphrase.php

--- corpus/findphrase.php
+++ corpus/findphrase.php
@@ -2,17 +2,22 @@
 if (isset($_POST['search'])) {
         $search = htmlentities($_POST['search']);
  
-$db = mysql_connect('us-cdbr-azure-west-c.cloudapp.net','b2a3214e88e413','325ebc40'); //Don't forget to change
-mysql_select_db('mysqldbproject', $db);             //theses parameters
-$sql = "SELECT expression from expressions_full WHERE expression LIKE '$search%'";
-$req = mysql_query($sql) or die();
+$con = mysqli_connect('us-cdbr-azure-west-c.cloudapp.net','b2a3214e88e413','325ebc40','mysqldbproject');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+    
+//Search results for echo ($q);
+mysqli_select_db($con,"mysqldbproject");
+$sql= "SELECT * FROM expressions_full WHERE  expression LIKE '$search%'";
+$req =  mysqli_query($con,$sql) or die();
 echo '<ul>';
-while ($data = mysql_fetch_array($req))
+while ($row = mysqli_fetch_array($req))
 {
-      echo '<li><a href="#" onclick="selected(this.innerHTML);">'.htmlentities($data['expression']).'</a></li>';
+      echo '<li><a href="#" onclick="selected(this.innerHTML);">'.htmlentities($row['expression']).'</a></li>';
 }
 echo '</ul>';
-mysql_close();
+mysqli_close($con);
 exit;
 }
 ?>
