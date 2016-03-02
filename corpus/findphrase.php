bdd0c6bfa0841d387d1f0ebf69ccd49df57a82a4
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2016-03-01 18:28:02 -0800

    Update findphrase.php

--- corpus/findphrase.php
+++ corpus/findphrase.php
@@ -1,98 +1,22 @@
-<!--<!DOCTYPE html>-->
-<!--<html>-->
-<!--<head>-->
-<!--<style type="text/css">-->
-<!--table {-->
-<!--    width: 100%;-->
-<!--    border-collapse: collapse;-->
-<!--}-->
-<!--table, td, th{-->
-<!--    border: 1px solid black;-->
-<!--    padding: 5px;-->
-<!--}-->
-<!--caption {-->
-<!--    display: table-caption;-->
-<!--    text-align: center;-->
-<!--}-->
-<!--th {text-align: left;}-->
-    
-<!--</style>-->
-<!--</head>-->
-<!--<body>-->
-
-
-
-<!--<?php-->
-//get the q parameter from URL
-<!--//$q=$_GET["q"];-->
-// connects to DB
-<!--//$con = mysqli_connect('us-cdbr-azure-west-c.cloudapp.net','b2a3214e88e413','325ebc40','mysqldbproject');-->
-<!--//if (!$con) {-->
-<!--  //  die('Could not connect: ' . mysqli_error($con));-->
-<!--}-->
-    
-    
-    
-    
-    
-//Search results for echo ($q);
-<!--//mysqli_select_db($con,"mysqldbproject");-->
-<!--//$sql= "SELECT * FROM expressions_full WHERE expression LIKE '%{$q}%'";-->
-//echo ($sql);  //Tests the sql statement
-<!--.//$result = mysqli_query($con,$sql);-->
-//Create table template
-// echo "<table>
-// <caption>Search Results:</caption>
-// <tr>
-// <th>student_id</th>
-// <th>expression</th>
-// <th>level_id</th>
-// <th>topic_id</th>
-// <th>language_id</th>
-// </tr>";
-//Input values into table
-<!--//while($row = mysqli_fetch_array($result)) {-->
-    // echo "<tr>";
-    // echo "<td>" . $row['expression_id'] . "</td>";
-<!-- //   $data = $row['expression'];-->
-<!--  //  echo "<td>" . $row['level_id'] . "</td>"-->
-    // echo "<td>" . $row['level_id'] . "</td>";
-    // echo "<td>" . $row['topic_id'] . "</td>";
-    // echo "<td>" . $row['language_id'] . "</td>";
-    // echo "</tr>";
-<!--}-->
-//output the table with values in it
-//echo "</table>";
-<!--//echo json_encode($data);-->
-<!--//mysqli_close($con);-->
-<!--?>-->
-<!--</body>-->
-<!--</html>-->
-
 <?php
 $dbHost = 'us-cdbr-azure-west-c.cloudapp.net';
 $dbUsername = 'b2a3214e88e413';
 $dbPassword = '325ebc40';
 $dbName = 'mysqldbproject';
 
-
-
-
-
-
-
-//connect with the database
-$db = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-//get search term
-$searchTerm = $_GET['term'];
-echo $searchTerm;
 //"SELECT * FROM expressions_full WHERE expression LIKE '%{$q}%'";
-//get matched data from skills table
-$query = $db->query("SELECT * FROM expressions_full WHERE expression LIKE '%".$searchTerm."%'");
-
-while ($row = $query->fetch_assoc()) {
-    $data[] = $row['expression'];
-}
-//return json data
-echo json_encode($data);
+	
+	$q=$_GET['q'];
+	$my_data=mysql_real_escape_string($q);
+	$mysqli=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName) or die("Database Error");
+	$sql="SELECT expression FROM expressions_full WHERE expression LIKE '%$my_data%'";
+	$result = mysqli_query($mysqli,$sql) or die(mysqli_error());
+	
+	if($result)
+	{
+		while($row=mysqli_fetch_array($result))
+		{
+			echo $row['name']."\n";
+		}
+	}
 ?>
