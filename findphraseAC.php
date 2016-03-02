ad522c706a2738ba57554898a80871fc25d3c304
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2016-03-01 18:42:44 -0800

    Update findphraseAC.php

--- findphraseAC.php
+++ findphraseAC.php
@@ -1,22 +1,45 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Auto Complete Input box</title>
-<link rel="stylesheet" type="text/css" href="jquery.autocomplete.css" />
-<script type="text/javascript" src="jquery.js"></script>
-<script type="text/javascript" src="jquery.autocomplete.js"></script>
+<TITLE>jQuery AJAX Autocomplete - Country Example</TITLE>
+<head>
+<style>
+body{width:610px;}
+.frmSearch {border: 1px solid #F0F0F0;background-color:#C8EEFD;margin: 2px 0px;padding:40px;}
+#country-list{float:left;list-style:none;margin:0;padding:0;width:190px;}
+#country-list li{padding: 10px; background:#FAFAFA;border-bottom:#F0F0F0 1px solid;}
+#country-list li:hover{background:#F0F0F0;}
+#search-box{padding: 10px;border: #F0F0F0 1px solid;}
+</style>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
 <script>
 $(document).ready(function(){
- $("#tag").autocomplete("findphrase.php", {
-		selectFirst: true
+	$("#search-box").keyup(function(){
+		$.ajax({
+		type: "POST",
+		url: "readCountry.php",
+		data:'keyword='+$(this).val(),
+		beforeSend: function(){
+			$("#search-box").css("background","#FFF url(LoaderIcon.gif) no-repeat 165px");
+		},
+		success: function(data){
+			$("#suggesstion-box").show();
+			$("#suggesstion-box").html(data);
+			$("#search-box").css("background","#FFF");
+		}
+		});
 	});
 });
+
+function selectCountry(val) {
+$("#search-box").val(val);
+$("#suggesstion-box").hide();
+}
 </script>
 </head>
-
 <body>
-    <label>Tag:</label>
-    <input name="tag" type="text" id="tag" size="20"/>
+<div class="frmSearch">
+<input type="text" id="search-box" placeholder="Country Name" />
+<div id="suggesstion-box"></div>
+</div>
 </body>
 </html>
