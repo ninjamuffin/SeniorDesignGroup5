af6b2343f46d0f576a3a1cf2815f1450f72b2977
brianramaswami <bramaswami@zagmail.gonzaga.edu>
2016-03-01 19:08:02 -0800

    Update findphrase.php

--- corpus/findphrase.php
+++ corpus/findphrase.php
@@ -1,22 +1,56 @@
 <?php
-$dbHost = 'us-cdbr-azure-west-c.cloudapp.net';
-$dbUsername = 'b2a3214e88e413';
-$dbPassword = '325ebc40';
-$dbName = 'mysqldbproject';
-
-//"SELECT * FROM expressions_full WHERE expression LIKE '%{$q}%'";
+class DBController {
+	private $host = "us-cdbr-azure-west-c.cloudapp.net";
+	private $user = "b2a3214e88e413";
+	private $password = "325ebc40";
+	private $database = "mysqldbproject";
 	
-	$q=$_GET['q'];
-	$my_data=mysql_real_escape_string($q);
-	$mysqli=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName) or die("Database Error");
-	$sql="SELECT expression FROM expressions_full WHERE expression LIKE '%$my_data%'";
-	$result = mysqli_query($mysqli,$sql) or die(mysqli_error());
-	
-	if($result)
-	{
-		while($row=mysqli_fetch_array($result))
-		{
-			echo $row['name']."\n";
+	function __construct() {
+		$conn = $this->connectDB();
+		if(!empty($conn)) {
+			$this->selectDB($conn);
 		}
 	}
+	
+	function connectDB() {
+		$conn = mysql_connect($this->host,$this->user,$this->password);
+		return $conn;
+	}
+	
+	function selectDB($conn) {
+		mysql_select_db($this->database,$conn);
+	}
+	
+	function runQuery($query) {
+		$result = mysql_query($query);
+		while($row=mysql_fetch_assoc($result)) {
+			$resultset[] = $row;
+		}		
+		if(!empty($resultset))
+			return $resultset;
+	}
+	
+	function numRows($query) {
+		$result  = mysql_query($query);
+		$rowcount = mysql_num_rows($result);
+		return $rowcount;	
+	}
+}
 ?>
+<!--"SELECT * FROM expressions_full WHERE expression LIKE '%{$q}%'";-->
+<?php
+//require_once("dbcontroller.php");
+$db_handle = new DBController();
+if(!empty($_POST["keyword"])) {
+$query =""SELECT * FROM expressions_full WHERE expression LIKE '" . $_POST["keyword"] . "%'";
+$result = $db_handle->runQuery($query);
+if(!empty($result)) {
+?>
+<ul id="country-list">
+<?php
+foreach($result as $country) {
+?>
+<li onClick="selectCountry('<?php echo $country["expression"]; ?>');"><?php echo $country["expression"]; ?></li>
+<?php } ?>
+</ul>
+<?php } } ?>
