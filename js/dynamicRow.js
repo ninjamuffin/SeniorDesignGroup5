89acc23d1edd31553a6c5279b79ee0497e472dc2
Menezes <rmenezes@zagmail.gonzaga.edu>
2016-04-04 20:07:40 -0700

    worksheet works no php yet

--- js/dynamicRow.js
+++ js/dynamicRow.js
@@ -1,16 +1,20 @@
 $(function(){
     // Find a <table> element with id="myTable":
     var exprTable = document.getElementById("myTable");
-    var corrExpr = document.getElementById("CorrectedExpr");
+    var corrExpr;
     
     $('#EditExpression').on('click',function(e) {
         var expr = exprTable.getElementsByTagName("td");
         document.getElementById("ExprToEdit").innerHTML= expr[1].innerHTML;
         document.getElementById("EditID").innerHTML= expr[0].innerHTML;
+        document.getElementById("Expression").innerHTML=expr[1].innerHTML;
+        $("#CorrectedExpr").val(expr[1].innerHTML);
     });
     
     $('#SubmitExpr').on('click',function(e){
+        corrExpr = document.getElementById("CorrectedExpr").value;
         document.getElementById("ExpressionStatus").innerHTML='Complete';
         document.getElementById("Expression").innerHTML=corrExpr;
+        $("#CorrectedExpr").val("");
     })
 });
--- student/MyCourses/ViewCourse/WorksheetEditor/index.php
+++ student/MyCourses/ViewCourse/WorksheetEditor/index.php
@@ -171,25 +171,32 @@ if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
                                         <div class="panel-heading">Expression Edit Window</div>
                                         <div class="panel-body">
                                             <div class="col-xs-8" name="left column">
-                                                <div class="col-xs-1" style="text-align: left";>
-                                                    #
-                                                </div>
-                                                <div class="col-xs-11" style="text-align: left";>
-                                                    Expression
-                                                </div>
-
-                                                <!--
-                                                    The following 2 div are used to dynamically call the expression to the edit window
-                                                -->
-                                                <div class="col-xs-1" style="text-align: left" id="EditID"></div>
-                                                <div class="col-xs-11" style="min-height: 100px" id="ExprToEdit">
-                                                </div>
                                                 
-                                                <div class="col-xs-12">
+                                                    <div class="col-xs-1" style="text-align: left";>
+                                                        #
+                                                    </div>
+                                                    <div class="col-xs-11" style="text-align: left";>
+                                                        Expression
+                                                    </div>
+
+                                                    <!--
+                                                        The following 2 div are used to dynamically call the expression to the edit window
+                                                    -->
+
+                                                    <div class="col-xs-1" style="text-align: left" id="EditID"></div>
+                                                    <div class="col-xs-11">
+                                                        <textarea disabled id="ExprToEdit" class="form-control" class="col-xs-11">
+                                                        </textarea>
+                                                    </div>
+                                                   
+                                                    
+                                               
+                                                
+                                                <div class="col-xs-12" style="padding-top: 40px">
                                                     <form role="form">
                                                         <div class="form-group">
                                                             <label for="CorrectedExpr">Correction:</label>
-                                                            <input type="text" class="form-control" id="CorrectedExpr" placeholder="Enter the correct expression here"/>
+                                                            <input type="text" class="form-control" id="CorrectedExpr" placeholder="Enter the correct expression here" />
                                                         </div>
                                                     </form>
                                                 </div>
